142ab023d85587ddc4ae04ea60fe4222f050699f
Siva <quic_sivb@quicinc.com>
2024-07-03 15:58:31 +0530

    Windows target definitions

--- ci/task/build_adreno_models_win.ps1
+++ ci/task/build_adreno_models_win.ps1
@@ -5,4 +5,4 @@ New-Item -ItemType Directory -Force -Path "./dist"
 New-Item -ItemType Directory -Force -Path "./dist/libs"
 
 python3 -m mlc_llm gen_config C:\sivb\MLC\models\Llama-2-7b-chat-hf --quantization q4f16_0 --conv-template llama-2 -o ./dist/Llama-2-7b-chat-hf-q4f16_0-MLC
-python3 -m mlc_llm compile ./dist/Llama-2-7b-chat-hf-q4f16_0-MLC/mlc-chat-config.json --device opencl -o ./dist/libs/Llama-2-7b-chat-hf-q4f16_0-windows.dll
+python3 -m mlc_llm compile ./dist/Llama-2-7b-chat-hf-q4f16_0-MLC/mlc-chat-config.json --device windows:adreno_x86 -o ./dist/libs/Llama-2-7b-chat-hf-q4f16_0-windows.dll
