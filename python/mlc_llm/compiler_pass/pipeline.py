05ef67fc7ac5a36e85bd11c8047e1539e3f01dcb
Siva <quic_sivb@quicinc.com>
2024-07-11 22:54:22 +0530

    AdrenoAcCL pass args

--- python/mlc_llm/compiler_pass/pipeline.py
+++ python/mlc_llm/compiler_pass/pipeline.py
@@ -133,7 +133,7 @@ def _mlc_llm_pipeline(  # pylint: disable=too-many-arguments
                 ),
                 FuseTransposeMatmul(),
                 # Adreno_Accl BYOC offloading.
-                PartitionForAdrenoACCL(),
+                PartitionForAdrenoACCL(target=target),
                 _DebugDump("debug-phase1.py", debug_dump, show_meta=False),
                 # Phase 2. Lowering to TIR, inherited TVM Relax's official "zero" pipeline
                 _LogProgress("Lowering to TVM TIR kernels"),
