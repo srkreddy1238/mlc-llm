e3617a4aae60deefe120b618de4a20ff04b3817d
B, Siva Rama Krishna Reddy <sivb@qti.qualcomm.com>
2024-08-01 19:13:10 +0530

    Scripts to terminate on error (#14)
    Co-authored-by: Siva <quic_sivb@quicinc.com>

--- ci/task/build_adreno_models_win.ps1
+++ ci/task/build_adreno_models_win.ps1
@@ -16,21 +16,30 @@ function build-model {
         [string]$template,
         [string]$addl_args
     )
-
     # Generate the model configuration
-    Invoke-Expression -Command  "python -m mlc_llm gen_config ${MODEL_LOCAL_BASE}\${model} --quantization ${quantization} --conv-template ${template} ${addl_args} -o ${MODEL_ARTIFACTS_PATH}\dist\${model}-${quantization}-MLC"
+    $global:LASTEXITCODE = 0
+    Invoke-Expression -Command  "python -m mlc_llm gen_config ${MODEL_LOCAL_BASE}\${model} --quantization ${quantization} --conv-template ${template} ${addl_args} -o ${MODEL_ARTIFACTS_PATH}\dist\${model}-${quantization}-MLC" -ErrorAction "Stop"
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
     # Convert the model weights
-    #Invoke-Expression -Command "python -m mlc_llm convert_weight ${MODEL_LOCAL_BASE}/${model} --quantization ${quantization} -o ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/ --device llvm"
+    # $global:LASTEXITCODE = 0
+    #Invoke-Expression -Command "python -m mlc_llm convert_weight ${MODEL_LOCAL_BASE}/${model} --quantization ${quantization} -o ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/ --device llvm" -ErrorAction "Stop"
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 
     # Compile the model for Adreno
-    Invoke-Expression -Command "python -m mlc_llm compile ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/mlc-chat-config.json --device windows:adreno_x86 -o ${MODEL_ARTIFACTS_PATH}/dist/libs/${model}-${quantization}-adreno.dll"
+    $global:LASTEXITCODE = 0
+    Invoke-Expression -Command "python -m mlc_llm compile ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/mlc-chat-config.json --device windows:adreno_x86 -o ${MODEL_ARTIFACTS_PATH}/dist/libs/${model}-${quantization}-adreno.dll" -ErrorAction "Stop"
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
     # Compile the model for Adreno with acceleration
-    Invoke-Expression -Command "python -m mlc_llm compile ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/mlc-chat-config.json --device windows:adreno_x86 --opt adrenoaccl=1 -o ${MODEL_ARTIFACTS_PATH}/dist/libs/${model}-${quantization}-adreno-accl.dll"
+    $global:LASTEXITCODE = 0
+    Invoke-Expression -Command "python -m mlc_llm compile ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/mlc-chat-config.json --device windows:adreno_x86 --opt adrenoaccl=1 -o ${MODEL_ARTIFACTS_PATH}/dist/libs/${model}-${quantization}-adreno-accl.dll" -ErrorAction "Stop"
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 }
 
 # Build the models
+
 build-model Llama-2-7b-chat-hf q4f16_0 llama-2 "--prefill-chunk-size 256"
 build-model Meta-Llama-3-8B-Instruct q4f16_0 llama-3 "--prefill-chunk-size 256"
 build-model Qwen-7B-Chat q4f16_0 chatml "--model-type qwen --prefill-chunk-size 256 --context-window-size 4096"
--- ci/task/build_adreno_models_win_nightly.ps1
+++ ci/task/build_adreno_models_win_nightly.ps1
@@ -15,10 +15,14 @@ function build-model {
     )
 
     # Compile the model for Adreno
+    $global:LASTEXITCODE = 0
     Invoke-Expression -Command "python -m mlc_llm compile ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/mlc-chat-config.json --device windows:adreno_x86 -o ./dist/libs/${model}-${quantization}-adreno.dll"
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
     # Compile the model for Adreno with acceleration
+    $global:LASTEXITCODE = 0
     Invoke-Expression -Command "python -m mlc_llm compile ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/mlc-chat-config.json --device windows:adreno_x86 --opt adrenoaccl=1 -o ./dist/libs/${model}-${quantization}-adreno-accl.dll"
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 }
 
 # Build the models
