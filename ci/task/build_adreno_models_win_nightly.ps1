5074890612ce2f5f470651e7a18c845375d5a87e
Vegiraju, Krishna Raju <kvegiraj@qti.qualcomm.com>
2025-05-02 10:58:10 +0530

    Update build_adreno_models_win_nightly.ps1

--- ci/task/build_adreno_models_win_nightly.ps1
+++ ci/task/build_adreno_models_win_nightly.ps1
@@ -44,6 +44,7 @@ function build-model {
       # Compile the model for Adreno with acceleration
       $global:LASTEXITCODE = 0
       Invoke-Expression -Command "python -m mlc_llm compile ${MODEL_ARTIFACTS_PATH}/dist/${model}-${quantization}-MLC/mlc-chat-config.json --device windows:adreno_x86 --opt openclml=1 -o ./dist/libs/${model}-${quantization}-adreno-clml.dll"
+      $global:LASTEXITCODE = 0
       #if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
     }
 }
