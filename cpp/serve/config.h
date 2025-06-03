780f88fd89b482fdd01ef79bc991a61fdbbdec3a
Vegiraju, Krishna Raju <kvegiraj@qti.qualcomm.com>
2025-06-03 20:40:57 +0530

    [CLI] Fix for mlc dll export (#63)
    Fix for mlc dll export
    
    Co-authored-by: krishnaraj36 <quic_kvegiraj@quicinc.com>

--- cpp/serve/config.h
+++ cpp/serve/config.h
@@ -320,7 +320,7 @@ class EngineConfigNode : public Object {
   TVM_FFI_DECLARE_OBJECT_INFO("mlc.serve.EngineConfig", EngineConfigNode, Object);
 };
 
-class EngineConfig : public ObjectRef {
+MLC_LLM_DLL class EngineConfig : public ObjectRef {
  public:
   /*! \brief Create EngineConfig from JSON object and inferred config. */
   static EngineConfig FromJSONAndInferredConfig(const tvm::ffi::json::Object& json,
