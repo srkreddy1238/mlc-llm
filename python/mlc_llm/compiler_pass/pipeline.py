62faba2d360470abb2a94d3a6d0c0ce41f1e2c63
krishnaraj36 <quic_kvegiraj@quicinc.com>
2026-04-01 09:56:01 +0530

    fix lint error

--- python/mlc_llm/compiler_pass/pipeline.py
+++ python/mlc_llm/compiler_pass/pipeline.py
@@ -6,11 +6,10 @@ from typing import Any, Dict, List, Optional
 import tvm
 from tvm import IRModule
 from tvm.relax import register_pipeline  # pylint: disable=no-name-in-module
-from tvm.relax.frontend import nn
-from tvm.s_tir import dlight as dl
 from tvm.relax.backend.adreno.clml import OpenCLMLOffLoadForLLM
 from tvm.relax.backend.contrib.adrenoaccl import PartitionForAdrenoACCL
 from tvm.relax.frontend import nn
+from tvm.s_tir import dlight as dl
 
 from mlc_llm.interface.compiler_flags import IPCAllReduceStrategyType
 from mlc_llm.support import logging
